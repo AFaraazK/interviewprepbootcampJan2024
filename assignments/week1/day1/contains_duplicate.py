3f4c47022cecea2eef43b78a2b7d99a0bbb7c6f6
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2024-01-02 15:14:27 -0500

    Create contains_duplicate.py

---
+++ assignments/week1/day1/contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        map1 = {}
+        for i in nums:
+            if i in map1:
+                return True
+            else:
+                map1[i] = True
+        return False
